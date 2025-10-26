b3918969d1e5a7d621d398078c931290e6610261
Rnchx <jpcrocha8@gmail.com>
2025-10-26 12:09:50 -0300

    add calculatePosFix function

---
+++ src/calculatePostfix.h
@@ -0,0 +1,10 @@
+#ifndef CALCULAR_POSFIXA_H
+#define CALCULAR_POSFIXA_H
+
+#include "basics.h"
+#include "fila.h"
+
+double calcular_posfixa(Fila* fila_posfixa);
+void testar_calculo_posfixa();
+
+#endif
